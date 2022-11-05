e0fb48849f8c44a5939cd3a202c271bc8c9a4ec1
Globox1997 <mitschke11@web.de>
2022-11-05 17:46:19 +0100

    fixed compat crash

--- src/main/java/draylar/tiered/mixin/compat/InfoGuiMixin.java
+++ src/main/java/draylar/tiered/mixin/compat/InfoGuiMixin.java
@@ -1,6 +1,6 @@
 package draylar.tiered.mixin.compat;
 
-import com.ibm.icu.text.DecimalFormat;
+import java.text.DecimalFormat;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
