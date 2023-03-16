57494f89afbda327ca3c84944b02762d6417e81f
Globox1997 <mitschke11@web.de>
2023-03-16 22:12:39 +0100

    added item frame and mob tiers

--- src/main/java/draylar/tiered/mixin/LootTableMixin.java
+++ src/main/java/draylar/tiered/mixin/LootTableMixin.java
@@ -1,6 +1,7 @@
 package draylar.tiered.mixin;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +21,18 @@ import net.minecraft.util.math.random.Random;
 
 @Mixin(LootTable.class)
 public class LootTableMixin {
+
+    @Inject(method = "method_331", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 0))
+    private static void processStacksMixin(Consumer<ItemStack> lootConsumer, ItemStack itemStack, CallbackInfo info) {
+        if (ConfigInit.CONFIG.lootContainerModifier)
+            ModifierUtils.setItemStackAttribute(null, itemStack, false);
+    }
+
+    @Inject(method = "method_331", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private static void processStacksMixin(Consumer<ItemStack> lootConsumer, ItemStack itemStack, CallbackInfo info, int i, ItemStack itemStack2) {
+        if (ConfigInit.CONFIG.lootContainerModifier)
+            ModifierUtils.setItemStackAttribute(null, itemStack2, false);
+    }
 
     @Inject(method = "supplyInventory", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/Inventory;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void supplyInventoryMixin(Inventory inventory, LootContext context, CallbackInfo info, ObjectArrayList<ItemStack> objectArrayList, Random random, List<Integer> list,
