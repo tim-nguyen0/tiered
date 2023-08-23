bcdc3c0a071f17fc2d4b6aa5f749887699846647
Globox1997 <mitschke11@web.de>
2023-08-23 09:53:43 +0200

    fixed equipment slot deserializer

--- src/main/java/draylar/tiered/gson/EquipmentSlotDeserializer.java
+++ src/main/java/draylar/tiered/gson/EquipmentSlotDeserializer.java
@@ -7,11 +7,12 @@ import com.google.gson.JsonParseException;
 import net.minecraft.entity.EquipmentSlot;
 
 import java.lang.reflect.Type;
+import java.util.Locale;
 
 public class EquipmentSlotDeserializer implements JsonDeserializer<EquipmentSlot> {
 
     @Override
     public EquipmentSlot deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return EquipmentSlot.byName(json.getAsString().toLowerCase());
+        return EquipmentSlot.byName(json.getAsString().toLowerCase(Locale.ROOT));
     }
 }
